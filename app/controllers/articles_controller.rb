e4be67329c6856c6c9c2ba2f1a065065940720cd
plok74122 <plok74122@gmail.com>
2016-08-29 14:18:52 +0800

    add action show , create

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,2 +1,16 @@
 class ArticlesController < ApplicationController
+
+  def show
+    article  = Article.find(params[:id])
+    render :json => article.to_json
+  end
+
+  def create
+    article = Article.new(:title => params[:title] , :content => params[:content])
+    if article.save
+      render :json => {:article => article , :message => "success"} , :status => 200
+    else
+      render :json => {:errors => article.errors , :message => "Invalid"}, :status => 400
+    end
+  end
 end

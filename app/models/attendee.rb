b06b593abcf6cb7b4ca46230f32eeeed9071826e
plok74122 <plok74122@gmail.com>
2016-08-31 11:54:05 +0800

    add relation between event and attendees

--- app/models/attendee.rb
+++ app/models/attendee.rb
@@ -1,2 +1,3 @@
 class Attendee < ApplicationRecord
+  belongs_to :event , :class_name => "Event", :foreign_key => "event_id"
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,4 @@
 class Event < ApplicationRecord
-  validates_presence_of :name , :description
+  validates_presence_of :name, :description
+  has_many :attendees, :class_name => "Attendee", :foreign_key => "event_id"
 end

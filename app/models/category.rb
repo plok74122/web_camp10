180e85f0a1ab890ba278b1fc56f61a11cb10b5f9
plok74122 <plok74122@gmail.com>
2016-08-31 12:45:53 +0800

    add relation between categroy and event

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,3 @@
 class Category < ApplicationRecord
+  has_many :events, :class_name => "Event", :foreign_key => "category_id"
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,5 @@
 class Event < ApplicationRecord
   validates_presence_of :name, :description
   has_many :attendees, :class_name => "Attendee", :foreign_key => "event_id"
+  belongs_to :category, :class_name => "Category", :foreign_key => "category_id"
 end
